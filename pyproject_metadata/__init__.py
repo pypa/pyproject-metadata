14dcc1de744a46c131e7a44335f2bf29384860f6
Henry Schreiner <HenrySchreinerIII@gmail.com>
2024-09-13 01:46:51 -0400

    chore: prepare for 0.9.0b1 (#147)
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- pyproject_metadata/__init__.py
+++ pyproject_metadata/__init__.py
@@ -34,7 +34,7 @@ import packaging.utils
 import packaging.version
 
 
-__version__ = '0.8.0'
+__version__ = '0.9.0b1'
 
 KNOWN_METADATA_VERSIONS = {'2.1', '2.2', '2.3', '2.4'}
 PRE_SPDX_METADATA_VERSIONS = {'2.1', '2.2', '2.3'}
