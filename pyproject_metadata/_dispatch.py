54d605a88d9ca8132716d970a061bf5442db867a
Henry Schreiner <henryfs@princeton.edu>
2025-11-20 11:47:00 -0500

    refactor: move tooling to _dispatch.py
    Signed-off-by: Henry Schreiner <henryfs@princeton.edu>

---
+++ pyproject_metadata/_dispatch.py
@@ -0,0 +1,82 @@
+# SPDX-License-Identifier: MIT
+
+"""
+This module contains dispatchers for use in project_table.py.
+"""
+
+from __future__ import annotations
+
+import functools
+import sys
+import typing
+from typing import Callable, Generic
+
+if sys.version_info < (3, 10):
+    if typing.TYPE_CHECKING:
+        from typing_extensions import ParamSpec
+    else:
+        ParamSpec = typing.TypeVar
+else:
+    from typing import ParamSpec
+
+
+__all__ = [
+    "get_name",
+    "is_typed_dict",
+    "valuedispatch",
+]
+
+
+def __dir__() -> list[str]:
+    return __all__
+
+
+P = ParamSpec("P")
+R = typing.TypeVar("R")
+
+
+class ValueDispatcher(Generic[P, R]):
+    def __init__(self, func: Callable[P, R]) -> None:
+        self.default: Callable[P, R] = func
+        self.registry: dict[object, Callable[P, R]] = {}
+        functools.update_wrapper(self, func)
+
+    def register(self, value: object) -> Callable[[Callable[P, R]], Callable[P, R]]:
+        """Register a function for an exact value."""
+
+        def decorator(func: Callable[P, R]) -> Callable[P, R]:
+            self.registry[value] = func
+            return func
+
+        return decorator
+
+    def dispatch(self, value: object) -> Callable[P, R]:
+        """Return the registered implementation or the default."""
+        return self.registry.get(value, self.default)
+
+    def __call__(self, value: object, *args: P.args, **kwargs: P.kwargs) -> R:
+        impl = self.dispatch(value)
+        return impl(*args, **kwargs)
+
+
+def valuedispatch(func: Callable[P, R]) -> ValueDispatcher[P, R]:
+    """Decorate a function into a ValueDispatcher."""
+    return ValueDispatcher(func)
+
+
+def is_typed_dict(type_hint: object) -> bool:
+    if sys.version_info >= (3, 10):
+        return typing.is_typeddict(type_hint)
+    return hasattr(type_hint, "__annotations__") and hasattr(type_hint, "__total__")
+
+
+def get_name(type_hint: type[object]) -> str:
+    """
+    Get the name of a type hint as a readable modern Python type.
+    """
+    if origin := typing.get_origin(type_hint):
+        if args := typing.get_args(type_hint):
+            arg_names = ", ".join(get_name(a) for a in args)
+            return f"{origin.__name__}[{arg_names}]"
+        return origin.__name__  # type: ignore[no-any-return]
+    return type_hint.__name__
