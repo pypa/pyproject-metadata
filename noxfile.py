2b992db474876ddc305c90ab92b9df6bbdc1ffb3
Henry Schreiner <HenrySchreinerIII@gmail.com>
2026-01-20 12:03:13 -0500

    ci: add downstream testing (#290)
    Signed-off-by: Henry Schreiner <henryfs@princeton.edu>

--- noxfile.py
+++ noxfile.py
@@ -6,6 +6,10 @@
 # ///
 
 import argparse
+import io
+import shutil
+import tarfile
+import urllib.request
 from pathlib import Path
 
 import nox
@@ -104,6 +108,58 @@ def docs(session: nox.Session) -> None:
         session.run("sphinx-autobuild", "--open-browser", *shared_args)
     else:
         session.run("sphinx-build", "--keep-going", *shared_args)
+
+
+PROJECTS = {
+    "sphinx-theme-builder": "https://github.com/pradyunsg/sphinx-theme-builder/archive/refs/tags/0.3.2.tar.gz",
+    "meson-python": "https://github.com/mesonbuild/meson-python/archive/refs/tags/0.19.0.tar.gz",
+    "scikit-build-core": "https://github.com/scikit-build/scikit-build-core/archive/41056e7b9aac3721994aa684de3314aa04c17dc9.tar.gz",
+    "pdm-backend": "https://github.com/pdm-project/pdm-backend/archive/refs/tags/2.4.6.tar.gz",
+}
+
+
+@nox.session(default=False)
+@nox.parametrize("project", list(PROJECTS))
+def downstream(session: nox.Session, project: str) -> None:
+    pkg_dir = Path.cwd() / "pyproject_metadata"
+    env = {"FORCE_COLOR": None}
+    session.install("-e.")
+
+    tmp_dir = Path(session.create_tmp())
+    session.chdir(tmp_dir)
+
+    shutil.rmtree(project, ignore_errors=True)
+    with urllib.request.urlopen(PROJECTS[project]) as resp:  # noqa: S310
+        data = resp.read()
+    with tarfile.open(fileobj=io.BytesIO(data), mode="r:gz") as tf:
+        tf.extractall(project)  # noqa: S202
+    (inner_dir,) = Path(project).iterdir()
+    session.chdir(inner_dir)
+
+    if project == "sphinx-theme-builder":
+        session.install("-r", "tests/requirements.txt")
+        session.install("-e.[cli]", "pip")
+        session.run("pip", "list")
+        session.run("pytest", "--pspec", "-knot import_name", env=env)
+    if project == "meson-python":
+        session.install("-e.", "--group=test", "pip")
+        session.run("pip", "list")
+        session.run("pytest", env=env)
+    if project == "pdm-backend":
+        session.install(
+            "-e.", "pytest", "pip", "pytest-gitconfig", "pytest-xdist", "setuptools"
+        )
+        session.run("pip", "list")
+        repl_dir = "src/pdm/backend/_vendor/pyproject_metadata"
+        shutil.rmtree(repl_dir)
+        shutil.copytree(pkg_dir, repl_dir)
+        session.run("pytest", "-knot get_version_from_scm", env=env)
+    if project == "scikit-build-core":
+        session.install("-e.", "--group=test")
+        repl_dir = "src/scikit_build_core/_vendor/pyproject_metadata"
+        shutil.rmtree(repl_dir)
+        shutil.copytree(pkg_dir, repl_dir)
+        session.run("pytest", "-mnot network", env=env)
 
 
 if __name__ == "__main__":
